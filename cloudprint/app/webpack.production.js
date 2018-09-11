91d7cc0bf0406e61a1b11b83d07c6be823c632fe
tianlun <375545256@qq.com>
2018-09-12 01:24:03 +0800

    update

--- cloudprint/app/webpack.production.js
+++ cloudprint/app/webpack.production.js
@@ -5,6 +5,7 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const tinypngCompress = require("webpack-tinypng-compress");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const stylesheetsLoaders = [{
   loader: 'css-loader',
@@ -38,18 +39,21 @@ function srcPathExtend(subpath) {
   return path.join(settings.srcPath, subpath)
 }
 
-const uglifyPlugin = new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } });
+//const uglifyPlugin = new webpack.optimize.UglifyJsPlugin({ minimize: true });
 const compressionPlugin = new CompressionPlugin();
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: 'false',
   context: path.join(__dirname, '/'),
-  entry: './index',
+  entry: {
+    vendor: ['react','react-dom','react-router-dom'], //在此处配置
+    bundle :__dirname + "/src/index.js"               //已多次提及的唯一入口文件
+  },
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath : "/",
-    filename: '[hash].js',
-    chunkFilename : "[id].[hash].bundle.chunk.js"
+    path: path.join(__dirname, 'build'),
+    publicPath : "./",
+    filename: '[name].js',
+    chunkFilename : "[id].[name].bundle.chunk.js"
   },
   externals: {
     'react': 'React'
@@ -72,11 +76,25 @@ module.exports = {
       beforeEmit: false
     }),
     definePlugin,
-    uglifyPlugin,
+    //uglifyPlugin,
     compressionPlugin
   ],
   resolve: {
     modules: ['node_modules', path.join(__dirname, 'src')]
+  },
+  optimization:{
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: 'all',
+          name: 'vendor',
+          priority: 10,
+          enforce: true
+        }
+      }
+    },
+    runtimeChunk: true
   },
   module: {
     rules: [
@@ -125,7 +143,52 @@ module.exports = {
             loader: 'less-loader'
           }]
         })
-      }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        use: [
+            {
+                loader: "file-loader",
+                options: {
+                    outputPath: "assets/"
+                }
+            }
+        ]
+      },
     ]
   },
   node: {
